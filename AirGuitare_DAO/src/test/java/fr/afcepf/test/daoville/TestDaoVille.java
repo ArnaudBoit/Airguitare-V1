d5b3ea6e94ff17ccdf018d2412c4b7728361032a
arnau <arnau@MSI>
2016-12-12 10:34:21 +0100

    tre

--- AirGuitare_DAO/src/test/java/fr/afcepf/test/daoville/TestDaoVille.java
+++ AirGuitare_DAO/src/test/java/fr/afcepf/test/daoville/TestDaoVille.java
@@ -1,5 +1,9 @@
 package fr.afcepf.test.daoville;
 
+import fr.afcepf.al29.airguitare.api.IDAOVille;
+import fr.afcepf.al29.airguitare.impl.DAOVIlle;
+
 public class TestDaoVille {
 
+	IDAOVille dao = new DAOVIlle();
 }
